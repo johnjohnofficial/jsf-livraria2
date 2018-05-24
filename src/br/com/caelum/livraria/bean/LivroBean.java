26802c1c835f9e827cc131fea44b2f3ac0561e7b
JohnJohn <jferreira.ba@gmail.com>
2018-05-24 03:11:10 -0300

    Aula 04 - Completando o sistema e lidando com escopos do JSF 2

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,17 +1,30 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Livro;
+import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
-public class LivroBean {
+@ViewScoped
+public class LivroBean implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private Livro livro = new Livro();
+	private Integer autorId;
 
 	public Livro getLivro() {
 		return livro;
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 
 	public void gravar() {
@@ -22,6 +35,29 @@ public class LivroBean {
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+	
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
 	}
 
 }
